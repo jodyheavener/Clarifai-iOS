694bd63cd05ea2f886f33b85f5c26e30b38db5ea
Jody Heavener <j.heavener@gmail.com>
2016-06-27 15:34:50 -0700

    Rewrite core library to be clearer and simpler to use. Does not include result parsing yet

--- Clarifai/Clarifai.swift
+++ Clarifai/Clarifai.swift
@@ -1,23 +1,13 @@
 import UIKit
 import Alamofire
 
-/** All available Clarifai recognition types */
-enum ClarifaiRecognitionType {
-    case Tag, Color
-}
-
-/** All available Models to apply to Clarifai Tag recognizition */
-enum ClarifaiTagModel {
-    case General, NSFW, Weddings, Travel, Food
-}
-
 /** Provides access to the Clarifai image recognition services */
 class Clarifai {
     var clientID: String
     var clientSecret: String
     var accessToken: String?
     var accessTokenExpiration: NSDate?
-    
+
     struct Config {
         static let BaseURL: String = "https://api.clarifai.com/v1"
         static let AppID: String = "com.clarifai.Clarifai.AppID"
@@ -26,33 +16,11 @@ class Clarifai {
         static let MinTokenLifetime: NSTimeInterval = 60
     }
     
-    enum DataInputType {
-        case Image, Video, URL
-    }
-    
     init(clientID: String, clientSecret: String) {
         self.clientID = clientID
         self.clientSecret = clientSecret
         
         self.loadAccessToken()
-    }
-    
-    // MARK: Public Methods
-    
-    func recognize(type: ClarifaiRecognitionType, image: UIImage, model: ClarifaiTagModel = .General, completion: (AnyObject?, NSError?) -> Void) {
-    
-    }
-    
-    func recognize(type: ClarifaiRecognitionType, images: Array<UIImage>, model: ClarifaiTagModel = .General, completion: (AnyObject?, NSError?) -> Void) {
-        
-    }
-    
-    func recognize(type: ClarifaiRecognitionType, url: String, model: ClarifaiTagModel = .General, completion: (AnyObject?, NSError?) -> Void) {
-        
-    }
-    
-    func recognize(type: ClarifaiRecognitionType, urls: Array<String>, model: ClarifaiTagModel = .General, completion: (AnyObject?, NSError?) -> Void) {
-        
     }
     
     // MARK: Access Token Management
@@ -129,7 +97,92 @@ class Clarifai {
         }
     }
     
-    // MARK: Helper Methods
+    // MARK: Recognition Processing
+    
+    /** All available Clarifai recognition types */
+    enum RecognitionType: String {
+        case Tag = "tag"
+        case Color = "color"
+    }
+    
+    /** All available Models to apply to Clarifai Tag recognizition */
+    enum TagModel: String {
+        case General = "general-v1.3"
+        case NSFW = "nsfw-v1.0"
+        case Weddings = "weddings-v1.0"
+        case Travel = "travel-v1.0"
+        case Food = "food-items-v0.1"
+    }
+    
+    /** All available ways to upload data to Clarifai for recognizition */
+    private enum DataInputType {
+        case Image, URL
+    }
+    
+    /** Recognize components of one or more images via UIImage */
+    func recognize(type: RecognitionType = .Tag, image: Array<UIImage>, model: TagModel = .General, completion: (AnyObject?, NSError?) -> Void) {
+        self.process(type, dataInputType: .Image, data: image, model: model, completion: completion)
+    }
+    
+    /** Recognize components of one or more images via string URL */
+    func recognize(type: RecognitionType = .Tag, url: Array<String>, model: TagModel = .General, completion: (AnyObject?, NSError?) -> Void) {
+        self.process(type, dataInputType: .URL, data: url, model: model, completion: completion)
+    }
 
+    private func process(type: RecognitionType, dataInputType: DataInputType, data: Array<AnyObject>, model: TagModel, completion: (AnyObject?, NSError?) -> Void) {
+        self.validateAccessToken { (error) in
+            if error != nil {
+                return completion(nil, error)
+            }
+            
+            let multiop = data.count > 1
+            let endpoint = multiop ? "/multiop" : "/\(type.rawValue)"
+            
+            Alamofire.upload(.POST, Config.BaseURL.stringByAppendingString(endpoint), headers: [
+                "Authorization": "Bearer \(self.accessToken!)"
+            ], multipartFormData: { multipartFormData in
+                if multiop {
+                    multipartFormData.appendBodyPart(data: type.rawValue.dataUsingEncoding(NSUTF8StringEncoding)!, name: "op")
+                }
+                
+                switch dataInputType {
+                case .URL:
+                    for url in data as! Array<String> {
+                        multipartFormData.appendBodyPart(data: url.dataUsingEncoding(NSUTF8StringEncoding)!, name: "url")
+                    }
+                case .Image:
+                    for image in data as! Array<UIImage> {
+                        let size = CGSizeMake(320, 320 * image.size.height / image.size.width)
+                        
+                        UIGraphicsBeginImageContext(size)
+                        image.drawInRect(CGRectMake(0, 0, size.width, size.height))
+                        let scaledImage = UIGraphicsGetImageFromCurrentImageContext()
+                        UIGraphicsEndImageContext()
+                        
+                        multipartFormData.appendBodyPart(data: UIImageJPEGRepresentation(scaledImage, 0.9)!, name: "encoded_image", fileName: "image.jpg", mimeType: "image/jpeg")
+                    }
+                }
+                
+                if type == .Tag {
+                    multipartFormData.appendBodyPart(data: model.rawValue.dataUsingEncoding(NSUTF8StringEncoding)!, name: "model")
+                }
+            }, encodingCompletion: { (encodingResult) in
+                switch encodingResult {
+                case .Success(let upload, _, _):
+                    upload.validate().responseJSON { response in
+                        switch response.result {
+                        case .Success(let result):
+//                            let results = ClarifaiResponse(type: type, dictionary: result as! Dictionary<NSObject, AnyObject>)
+                            completion(result, nil)
+                        case .Failure(let error):
+                            completion(nil, error)
+                        }
+                    }
+                case .Failure(let encodingError):
+                    print(encodingError)
+                }
+            })
+        }
+    }
     
 }
